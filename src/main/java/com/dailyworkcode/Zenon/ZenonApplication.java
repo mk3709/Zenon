fe28365e196edac078c37f7cc4514aa2d2450bd8
mk3709 <mk3709@srmist.edu.in>
2025-01-12 14:20:49 +0530

    ZenonApp

---
+++ src/main/java/com/dailyworkcode/Zenon/ZenonApplication.java
@@ -0,0 +1,13 @@
+package com.dailyworkcode.Zenon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailyworkcode/Zenon/ZenonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyworkcode.Zenon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
